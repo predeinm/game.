fb084d2fc224f7a365483cd4d891f363d35ae7bb
Oleg518 <predeinm@internet.ru>
2026-06-18 16:17:19 +0500

    Update input_handler.py

--- core/input_handler.py
+++ core/input_handler.py
@@ -1,16 +1,13 @@
-# core/input_handler.py
 import pygame
 
 class InputHandler:
-    """Централизованная обработка ввода."""
     
     def __init__(self):
-        self.keys_pressed = None  # Будет кортеж от pygame
+        self.keys_pressed = None 
         self.mouse_pos = (0, 0)
         self.mouse_buttons = {}
 
     def update(self, events):
-        """Обновить состояние ввода."""
         # pygame.key.get_pressed() возвращает последовательность, не словарь!
         self.keys_pressed = pygame.key.get_pressed()
         self.mouse_pos = pygame.mouse.get_pos()
@@ -22,12 +19,9 @@ class InputHandler:
                 self.mouse_buttons[event.button] = False
 
     def is_key_pressed(self, key):
-        """Проверить, нажата ли клавиша."""
-        # Доступ по индексу, а не .get()!
         if self.keys_pressed is None:
             return False
         return bool(self.keys_pressed[key])
 
     def get_mouse_pos(self):
-        """Получить позицию мыши."""
         return self.mouse_pos
